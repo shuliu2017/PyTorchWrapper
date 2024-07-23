a1484e537945fc964d7f35bf389adb38ec048d0a
Shu Liu <liushu2011@outlook.com>
2024-07-22 21:31:15 -0400

    Update customized_metrics.py

--- packages/customized_metrics.py
+++ packages/customized_metrics.py
@@ -1,4 +1,4 @@
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, mean_squared_error
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, mean_squared_error, r2_score, mean_absolute_error
 
 classification_metrics = {
     'accuracy': (accuracy_score, {}),
@@ -8,14 +8,7 @@ classification_metrics = {
 }
 
 regression_metrics = {
-    'mean_squared_error': (mean_squared_error, {})
+    'mean_squared_error': (mean_squared_error, {}),
+    'r2_score': (r2_score, {}),
+    'mean_absolute_error': (mean_absolute_error, {})
 }
-
-"""
-import torch
-
-def accuracy(targets, preds):
-  
-  accuracy = (torch.tensor(preds) == torch.tensor(targets)).sum().item() / len(targets)
-  return accuracy
-"""
