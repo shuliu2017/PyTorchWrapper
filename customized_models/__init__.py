4861ae26bcf9caeeb48a7f8711caceedd76c794d
Shu Liu <liushu2011@outlook.com>
2024-07-22 20:21:31 -0400

    Create __init__.py

---
+++ customized_models/__init__.py
@@ -0,0 +1,6 @@
+# customized_models/__init__.py
+
+import customized_models.vit_regressor
+import customized_models.transfer_learning_torchvision
+
+__all__ = ['vit_regressor', 'transfer_learning_torchvision']
