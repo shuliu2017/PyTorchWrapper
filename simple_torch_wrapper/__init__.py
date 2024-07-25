a29aae223b0db99f0c6175c21f2732bb2e533ba6
Shu Liu <liushu2011@outlook.com>
2024-07-25 17:14:04 -0400

    Update __init__.py

--- simple_torch_wrapper/__init__.py
+++ simple_torch_wrapper/__init__.py
@@ -4,5 +4,6 @@ from . import pytorch_tools
 from . import workflow
 from . import customized_metrics
 from . import cv_utils
+from . import models
 
-__all__ = ['pytorch_tools', 'workflow', 'customized_metrics', 'cv_utils']
+__all__ = ['pytorch_tools', 'workflow', 'customized_metrics', 'cv_utils', 'models']
