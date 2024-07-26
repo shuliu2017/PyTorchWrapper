4d8357c0c586101aa72df2694b875fb628a280a4
Shu Liu <liushu2011@outlook.com>
2024-07-25 20:40:12 -0400

    Update __init__.py

--- simple_torch_wrapper/models/__init__.py
+++ simple_torch_wrapper/models/__init__.py
@@ -2,30 +2,34 @@
 
 import importlib
 import sys
-import os
 
-class LazyLoader:
-    def __init__(self, module_name):
+class LazyModule:
+    def __init__(self, module_name, package):
+        # full module path
         self.module_name = module_name
+        # the package which the module resides 
+        self.package = package
         self.module = None
 
     def _load(self):
         if self.module is None:
-            self.module = importlib.import_module(f'.{self.module_name}', package='simple_torch_wrapper.models')
+            self.module = importlib.import_module(self.module_name, self.package)
         return self.module
 
-    def __getattr__(self, item):
-        return getattr(self._load(), item)
+    def __getattr__(self, attr):
+        return getattr(self._load(), attr)
 
-# Get the directory of the current file
-current_dir = os.path.dirname(__file__)
+# Manually add the models for lazy loading
+transfer_learning_torchvision = LazyModule('simple_torch_wrapper.models.transfer_learning_torchvision', 'simple_torch_wrapper.models')
+vit_regressor = LazyModule('simple_torch_wrapper.models.vit_regressor', 'simple_torch_wrapper.models')
 
-# Initialize the __all__ variable
-__all__ = []
+# Add the lazy loaders to sys.modules
+sys.modules['simple_torch_wrapper.models.transfer_learning_torchvision'] = transfer_learning_torchvision
+sys.modules['simple_torch_wrapper.models.vit_regressor'] = vit_regressor
 
-# Loop through all the files in the directory
-for filename in os.listdir(current_dir):
-    # Check if the file is a Python file but not __init__.py
-    if filename.endswith('.py') and filename != '__init__.py':
-        # Get the module name by removing the file extension
-        module_name = filename
+# Also set the modules as attributes of models for direct access
+setattr(sys.modules[__name__], 'transfer_learning_torchvision', transfer_learning_torchvision)
+setattr(sys.modules[__name__], 'vit_regressor', vit_regressor)
+
+# Define __all__ for explicit imports
+__all__ = ['transfer_learning_torchvision', 'vit_regressor']
